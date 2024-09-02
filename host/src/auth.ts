dc000f069cad6dd0b316ada4a3df664d4fd4f083
Vann <111390478+Van-x2@users.noreply.github.com>
2024-09-02 09:53:56 +0000

    fixed auth js

--- host/src/auth.ts
+++ host/src/auth.ts
@@ -1,9 +1,18 @@
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "$env/static/private"
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/sveltekit/providers/github"
+
+
+export const authOptions = {
+  // Add trusted hosts here
+  trustedHosts: ['localhost:4173']
+  
+};
  
 export const { handle } = SvelteKitAuth({
   providers: [
     GitHub({clientId: GITHUB_CLIENT_ID, clientSecret: GITHUB_CLIENT_SECRET})
   ],
+  trustHost: true
+  
 })
