06fec978d822834151dc0a5d309b932f7e2300e7
Vann <111390478+Van-x2@users.noreply.github.com>
2024-10-20 22:25:54 -1000

    Update mongoconnect.ts

--- host/src/lib/mongoconnect.ts
+++ host/src/lib/mongoconnect.ts
@@ -4,39 +4,26 @@ import { MONGO_STRING } from "$env/static/private"
 let client: MongoClient | null = null
 
 export async function connectToDatabase() {
-    if (client && client.topology?.isConnected()) {
-        console.log('Connected to MongoDB on existing connection')
+    if(client && client.topology?.isConnected()) {
+        console.log('connected to MongoDB on existing connection')
         return client
     }
 
-    try {
-        console.log('Starting new connection to MongoDB')
-        client = new MongoClient(MONGO_STRING)
-        await client.connect()
-        return client
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error)
-        // If the topology is closed, attempt to reconnect
-        if (error.message.includes("Topology is closed")) {
-            console.log('Reconnecting to MongoDB')
-            client = new MongoClient(MONGO_STRING)
-            await client.connect()
-            return client
-        }
-        throw error // Re-throw if it's a different error
-    }
+    console.log('starting new connection to mongo')
+    client = new MongoClient(MONGO_STRING)
+    await client.connect()
+    return client
 }
 
-export async function getClient() {
-    if (!client || !client.topology?.isConnected()) {
-        console.log('Client is not connected, creating a new connection')
-        return await connectToDatabase()
+export function getClient() {
+    if (!client) {
+        throw new Error('Call connectToDatabasefirst')
     }
     return client
 }
 
 export async function closeDatabase() {
-    if (client) {
+    if(client) {
         await client.close()
         client = null
         console.log('Disconnected from MongoDB')
