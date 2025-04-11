241a9c8f279ad2575ec8bb516111e33e007cc2db
Vann <111390478+Van-x2@users.noreply.github.com>
2025-04-11 19:48:19 +0000

    The account deletion proccess now includes deleting customer from stripe backend.

--- host/src/routes/api/deleteaccount/+server.ts
+++ host/src/routes/api/deleteaccount/+server.ts
@@ -1,12 +1,16 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from '@sveltejs/kit';
-import { MONGO_STRING, MAILGUN_SECRET, MAILGUN_DOMAIN } from '$env/static/private';  // Assume you have MAILGUN_DOMAIN
+import { MONGO_STRING, MAILGUN_SECRET, MAILGUN_DOMAIN, STRIPE_SECRET_KEY } from '$env/static/private';  // Assume you have MAILGUN_DOMAIN
 import { ObjectId } from 'mongodb';
 import { getClient } from '$lib/mongoconnect';
 import formData from 'form-data';
 import { signOut } from '@auth/sveltekit/client';
+import Stripe from 'stripe';
 
 export const POST: RequestHandler = async ({ request }) => {
+
+  //Setups up stripe body
+  const stripe = new Stripe(STRIPE_SECRET_KEY)
 
   // Deals with turning request body into usable data
   const body = await request.json()
@@ -16,6 +20,9 @@ export const POST: RequestHandler = async ({ request }) => {
 
   const db = client.db('Users')
   const MongoCollection = db.collection('hosts')
+
+  //deletes customer from the stripe side of things, cancelling any subscriptions in the process
+  await stripe.customers.del(body.stripeid)
 
   const response = await MongoCollection.deleteOne(
     { _id: new ObjectId(body.id) }
